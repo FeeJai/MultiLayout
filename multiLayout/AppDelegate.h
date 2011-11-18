d4d809950b145fcd1ee1d4bb4f0c7d9afe99ca6d
Felix Jankowski <github@jankowski.me>
2011-11-18 22:12:02 +0100

    Initial Commit

---
+++ multiLayout/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  multiLayout
+//
+//  Created by Felix Jankowski on 18.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ multiLayout/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  multiLayout
+//
+//  Created by Felix Jankowski on 18.11.11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
